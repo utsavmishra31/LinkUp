f39edb98461ef60f8eb073a1e20a0318bd6c0bcd
Utsav <utsav@Duco2s-MacBook-Air.local>
2025-12-29 16:24:20 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
+import '../global.css';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
