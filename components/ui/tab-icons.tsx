b7bff2337deee3f3f86c6af1976a413de4085b37
Utsav Mishra <utsavmishra1979@gmail.com>
2026-01-23 14:08:15 +0530

    x icon added in global

--- components/ui/tab-icons.tsx
+++ components/ui/tab-icons.tsx
@@ -40,25 +40,10 @@ export const LikeIcon = ({ color, size = 24, focused }: IconProps) => {
 export const GlobalIcon = ({ color, size = 24, focused }: IconProps) => {
     return (
         <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
-            <Circle
-                cx="12"
-                cy="12"
-                r="10"
-                fill={focused ? color : 'none'}
+            <Path
+                d="M3 3L21 21M21 3L3 21"
                 stroke={color}
-                strokeWidth={focused ? 0 : 2}
-            />
-            <Path
-                d="M2 12H22"
-                stroke={focused ? 'white' : color}
-                strokeWidth={2}
-                strokeLinecap="round"
-            />
-            <Path
-                d="M12 2C14.5013 4.73835 15.9228 8.29203 16 12C15.9228 15.708 14.5013 19.2616 12 22C9.49872 19.2616 8.07725 15.708 8 12C8.07725 8.29203 9.49872 4.73835 12 2Z"
-                fill={focused ? 'none' : 'none'}
-                stroke={focused ? 'white' : color}
-                strokeWidth={2}
+                strokeWidth={3}
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
